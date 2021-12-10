494eb0540a3e4d939c370c7905cc20446d93981b
Abraham cahyo adi <39833970+161134077@users.noreply.github.com>
2021-12-11 03:19:28 +0700

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,12 @@
+cd /usr/local/directadmin/custombuild
+./build update
+./build set php1_release 7.0
+./build set php2_release 7.3
+./build set php3_release 7.4
+./build set php4_release 8.0
+./build set php1_mode php-fpm
+./build set php2_mode php-fpm
+./build set php3_mode php-fpm
+./build set php4_mode php-fpm
+./build php n
+./build rewrite_confs
